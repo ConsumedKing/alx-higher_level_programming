6dca51daff4b55614d39ad590a36e07633d30941
Mustafa-cpp <mustafa_165953@fci.sohag.edu.eg>
2023-09-12 00:48:10 +0300

    B/astards?

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
     """ My function that prints integers """
     for i in my_list:
-        print('{0:d}'.format(i))
+        print('{:d}'.format(i))
