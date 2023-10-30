5ebbc590975f715f2d426a4431a94ecdbbbb6a45
ConsumedKing <mustafa_165953@fci.sohag.edu.eg>
2023-10-30 13:52:59 +0200

    Simple as that

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,19 +7,23 @@ Class Rectangle Real Definition
 class Rectangle:
     """ Rectangle Class """
     def __init__(self, width=0, height=0):
+        """ init """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """ width getter """
         return self.__width
 
     @property
     def height(self):
+        """ height getter """
         return self.__height
 
     @width.setter
     def width(self, value):
+        """ width setter """
         if value < 0:
             raise ValueError("width must be >= 0")
         if not isinstance(value, int):
@@ -28,6 +32,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """ height setter """
         if value < 0:
             raise ValueError("height must be >= 0")
         if not isinstance(value, int):
