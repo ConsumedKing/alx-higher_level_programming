19d24b08adc632bfe9f19f9d4f4672cd2313dfc1
Mustafa Nasser <mustafa_165953@fci.sohag.edu.eg>
2024-02-17 15:43:08 +0200

    Python 3.8.6

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is Script two in mysql project
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost",
+                                 user=sys.argv[1],
+                                 password=sys.argv[2],
+                                 database=sys.argv[3],
+                                 port=3306)
+    cursor = connection.cursor()
+    query = f"SELECT id, name FROM states WHERE name LIKE BINARY\
+        '{sys.argv[4]}' ORDER BY id ASC"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    connection.close()
