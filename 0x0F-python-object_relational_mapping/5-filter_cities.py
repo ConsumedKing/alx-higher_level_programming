c6932c2a29227b0d46d721c40ba6e74e94d2a448
Mustafa Nasser <mustafa_165953@fci.sohag.edu.eg>
2024-02-17 18:57:29 +0200

    5-filter_cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,13 +12,22 @@ if __name__ == "__main__":
                                  database=sys.argv[3],
                                  port=3306)
     cursor = connection.cursor()
-    cursor.execute("SELECT cities.name \
-                   FROM states, cities WHERE states.id = cities.state_id \
-                   AND states.name LIKE BINARY %s ORDER BY cities.id ASC",
+    cursor.execute("""SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %s
+            ORDER BY
+                cities.id ASC""",
                    (sys.argv[4], ))
     results = cursor.fetchall()
     for i in range(len(results)):
-        print(results[i][0] + ', ', end="") if i < len(results) - 1 else \
-            print(results[i][0])
+        print(results[i][1] + ', ', end="") if i < len(results) - 1 else \
+            print(results[i][1])
     cursor.close()
     connection.close()
