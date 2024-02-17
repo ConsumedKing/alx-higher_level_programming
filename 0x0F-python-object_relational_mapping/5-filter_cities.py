d7ec361537cf4d55f1cc87ce8eb02e99819024a2
Mustafa Nasser <mustafa_165953@fci.sohag.edu.eg>
2024-02-17 18:19:05 +0200

    5-filter_cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is Script two in mysql project
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost",
+                                 user=sys.argv[1],
+                                 password=sys.argv[2],
+                                 database=sys.argv[3],
+                                 port=3306)
+    cursor = connection.cursor()
+    cursor.execute("SELECT cities.id, cities.name ,states.name \
+                   FROM states, cities WHERE states.id = cities.state_id \
+                   AND states.name = %s", (sys.argv[4], ))
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    connection.close()
