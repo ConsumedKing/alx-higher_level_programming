3ba9eea8fbb7193beeb6f05269e8ab9815516914
Mustafa Nasser <mustafa_165953@fci.sohag.edu.eg>
2024-02-17 18:27:21 +0200

    5-filter_cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,11 +12,12 @@ if __name__ == "__main__":
                                  database=sys.argv[3],
                                  port=3306)
     cursor = connection.cursor()
-    cursor.execute("SELECT cities.id, cities.name ,states.name \
+    cursor.execute("SELECT cities.name \
                    FROM states, cities WHERE states.id = cities.state_id \
                    AND states.name = %s", (sys.argv[4], ))
     results = cursor.fetchall()
-    for row in results:
-        print(row)
+    for i in range(len(results)):
+        print(results[i][0] + ', ', end="") if i < len(results) - 1 else \
+            print(results[i][0])
     cursor.close()
     connection.close()
