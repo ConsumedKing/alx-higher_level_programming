1abb74a9a863f9b5ec7943ea7ac8b8e915e9586a
Mustafa Nasser <mustafa_165953@fci.sohag.edu.eg>
2024-02-18 15:09:38 +0200

    6-model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,14 +8,16 @@ import sys
 
 Base = declarative_base()
 
+
 class State(Base):
     """tate class:
         inherits from Base Tips
     links to the MySQL table states
-    class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
-    class attribute name that represents a column of a string with maximum 128 characters and can’t be null
+    class attribute id that represents a column of an auto-generated, \
+        unique integer, can’t be null and is a primary key
+    class attribute name that represents a column of a string with maximum 128\
+          characters and can’t be null
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name =  Column(String(128), nullable=False)
-
+    name = Column(String(128), nullable=False)
