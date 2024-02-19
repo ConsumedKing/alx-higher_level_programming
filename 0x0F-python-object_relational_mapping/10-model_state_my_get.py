918cdd26a04801f72864adf27db2d145a3b1ee0c
Mustafa Nasser <mustafa_165953@fci.sohag.edu.eg>
2024-02-19 03:32:19 +0200

    10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is Script two in mysql project
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(
+        State.name.like(sys.argv[4])).order_by(State.id)
+    if not states:
+        print("Not Found")
+    else:
+        for state in states:
+            print(f'{state.id}')
