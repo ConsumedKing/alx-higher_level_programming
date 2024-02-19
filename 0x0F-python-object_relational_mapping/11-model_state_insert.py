d16d7cffadfa5f9c5e7894a206e8c3428d475e5b
Mustafa Nasser <mustafa_165953@fci.sohag.edu.eg>
2024-02-19 03:48:40 +0200

    11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is Script two in mysql project
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    Louisiana = State(name="Louisiana")
+    session.add(Louisiana)
+    session.commit()
